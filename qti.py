a1123deb5d26a20df4aa254cd8829c483a902d77
Utkarsh G <34621440+UtkarshCrazy@users.noreply.github.com>
2020-06-12 14:48:01 -0400

    Delete qti.py

--- qti.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(300,300)
-    w.setWindowTitle("Hyperloop")
-    w.show()
-    sys.exit(app.exec_())
